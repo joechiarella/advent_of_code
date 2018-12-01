08282b574af4a7d8f07f7d8d0750b9e1c0392ee8
Joe Chiarella <joe.chiarella@salesloft.com>
2018-12-01 09:31:07 -0500

    Switched to infinite stream and reduce

--- lib/aoc2018_1b.ex
+++ lib/aoc2018_1b.ex
@@ -3,23 +3,15 @@ defmodule AOC2018_1B do
     input
     |> String.split
     |> Enum.map(&String.to_integer/1)
-    |> find_first_duplicate
+    |> Stream.cycle
+    |> Enum.reduce_while({0, MapSet.new}, &find_first_duplicate/2)
   end
 
-  def find_first_duplicate(list) do
-    find_first_duplicate(list, list, 0, MapSet.new)
-  end
-
-  def find_first_duplicate([first | rest], full_list, current, previous) do
+  def find_first_duplicate(first, {current, previous}) do
     if MapSet.member?(previous, current) do
-      current
+      {:halt, current}
     else
-      find_first_duplicate(rest, full_list, current + first, MapSet.put(previous, current))
+      {:cont, {current + first, MapSet.put(previous, current)}}
     end
   end
-
-  def find_first_duplicate([], full_list, current, previous) do
-    find_first_duplicate(full_list, full_list, current, previous)
-  end
-
 end
