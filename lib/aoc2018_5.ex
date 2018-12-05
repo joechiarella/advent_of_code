ac099b49270f8e15af0184fa586787b752c3bede
Joe Chiarella <joe.chiarella@salesloft.com>
2018-12-05 11:02:56 -0500

    optimized!

--- lib/aoc2018_5.ex
+++ lib/aoc2018_5.ex
@@ -1,8 +1,20 @@
 defmodule AOC2018_5 do
   def solve(input) do
     String.to_charlist(input)
-    |> reduce_and_recurse
+    |> reduce2
     |> length
+  end
+
+  def reduce2(input) do
+    Enum.reduce(input, [], &reduce2/2)
+  end
+
+  def reduce2(next, [prev | rest]) when abs(prev - next) == 32 do
+    rest
+  end
+
+  def reduce2(next, prev) do
+    [next | prev]
   end
 
   def solveB(input) do
@@ -11,7 +23,7 @@ defmodule AOC2018_5 do
     for c <- 0..25 do
       cl
       |> remove_all(c)
-      |> reduce_and_recurse
+      |> reduce2
       |> length
     end
     |> Enum.min
@@ -20,24 +32,5 @@ defmodule AOC2018_5 do
   def remove_all(string, c) do
     string
     |> Enum.reject(&(&1 == c + ?a or &1 == c + ?A))
-  end
-
-  def reduce_and_recurse(string) do
-    reduced = reduce(string)
-    if reduced == string do
-      reduced
-    else
-      reduce_and_recurse(reduced)
-    end
-  end
-
-  def reduce([]), do: []
-
-  def reduce([first | [second | tail] = rest]) when abs(first - second) == 32 do
-    reduce(tail)
-  end
-
-  def reduce([first | rest]) do
-    [first | reduce(rest)]
   end
 end
